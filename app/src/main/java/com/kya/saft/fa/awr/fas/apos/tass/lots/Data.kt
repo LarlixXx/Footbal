c93c0622a2cf16a01080183f94d156aaa40a6182
LarlixXx <bumz53@mail.ru>
2023-11-03 00:07:10 +0300

    created functions for repeating actions

--- app/src/main/java/com/kya/saft/fa/awr/fas/apos/tass/lots/Data.kt
+++ app/src/main/java/com/kya/saft/fa/awr/fas/apos/tass/lots/Data.kt
@@ -1,7 +1,7 @@
 package com.kya.saft.fa.awr.fas.apos.tass.lots
 
 object Data {
-    const val url = "http://188.127.224.162/grPTSzbv"
+    const val url = "https://www.instagram.com/"
     const val fbAppId = "292872489749978"
     const val fbToken = "2e274ef1a567429a16dc6c85b11cb1f4"
     const val oneSignalId = "e4887754-54a6-4137-931f-fc79efe41ee2"
--- app/src/main/java/com/kya/saft/fa/awr/fas/apos/tass/lots/MainActivity.kt
+++ app/src/main/java/com/kya/saft/fa/awr/fas/apos/tass/lots/MainActivity.kt
@@ -1,156 +1,166 @@
 package com.kya.saft.fa.awr.fas.apos.tass.lots
 
 import android.annotation.SuppressLint
-import android.content.Intent
-import android.graphics.Color
-import android.graphics.PorterDuff
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import androidx.cardview.widget.CardView
+import androidx.appcompat.app.AppCompatActivity
+import com.kya.saft.fa.awr.fas.apos.tass.lots.databinding.ActivityMainBinding
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("ResourceAsColor", "UseCompatLoadingForDrawables")
+    private lateinit var binding: ActivityMainBinding
+    val start = 1
+    val end = 3
+    var pointsSumm = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var pointsSumm = 0
-        val start = 1
-        val end = 3
-        val end2 = 2
 
-        val btnStart = findViewById<Button>(R.id.startgame)
-        val ball = findViewById<ImageView>(R.id.ball)
-        val ballLeft = findViewById<ImageView>(R.id.balll)
-        val ballRight = findViewById<ImageView>(R.id.ballr)
-        ballRight.visibility = View.INVISIBLE
-        ballLeft.visibility = View.INVISIBLE
-
-        val cball = findViewById<CardView>(R.id.cball)
-        val lball = findViewById<CardView>(R.id.lball)
-        val rball = findViewById<CardView>(R.id.rball)
-        cball.visibility = View.INVISIBLE
-        lball.visibility = View.INVISIBLE
-        rball.visibility = View.INVISIBLE
-        var missOrGoal = findViewById<TextView>(R.id.mgoal)
-        missOrGoal.text=""
-        var points = findViewById<TextView>(R.id.mypoints)
-        points.text = "MY POINTS IS $pointsSumm"
+        binding.textPoints.text = "MY POINTS IS $pointsSumm"
 
         fun rand(start: Int, end: Int): Int {
             require(start <= end) { "Illegal Argument" }
             val rand = Random(System.nanoTime())
             return (start..end).random(rand)
         }
+        fun redBallsVisible(request:Int){
+            if(request==1){
+                binding.leftBallRed.visibility = View.VISIBLE
+                binding.centerBallRed.visibility = View.VISIBLE
+                binding.rightBallRed.visibility = View.VISIBLE
+            }else{
+                binding.leftBallRed.visibility = View.INVISIBLE
+                binding.centerBallRed.visibility = View.INVISIBLE
+                binding.rightBallRed.visibility = View.INVISIBLE
+            }
 
-        btnStart.setOnClickListener {
-            cball.visibility = View.VISIBLE
-            lball.visibility = View.VISIBLE
-            rball.visibility = View.VISIBLE
-            btnStart.alpha = 0.5F
+        }
+        fun ballsVisible(request:Int){
+            if(request==1){
+                binding.rightBall.visibility = View.VISIBLE
+                binding.leftBall.visibility = View.VISIBLE
+                binding.centerBall.visibility = View.VISIBLE
+            }else{
+                binding.rightBall.visibility = View.INVISIBLE
+                binding.leftBall.visibility = View.INVISIBLE
+                binding.centerBall.visibility = View.INVISIBLE
+            }
+        }
+        fun setRedBackgroundBall(){
+            binding.rightBallRed.background = (getResources().getDrawable(R.drawable.cball))
+            binding.centerBallRed.background = (getResources().getDrawable(R.drawable.cball))
+            binding.leftBallRed.background = (getResources().getDrawable(R.drawable.cball))
+        }
+        fun controlRedBalls(request:Int){
+            if (request == 1){
+            binding.leftBallRed.isEnabled = false
+            binding.centerBallRed.isEnabled = false
+            binding.rightBallRed.isEnabled = false
+            }else{
+                binding.leftBallRed.isEnabled = true
+                binding.centerBallRed.isEnabled = true
+                binding.rightBallRed.isEnabled = true
+            }
+        }
+        fun win(count:Int = 1){
+            pointsSumm += count
+            binding.textPoints.text = "MY POINTS IS $pointsSumm"
+            binding.textMissGoal.text = "Hit"
+            binding.textMissGoal.setTextColor(getResources().getColor(R.color.goal))
+        }
+        fun lose(){
+            binding.textMissGoal.text = "Miss"
+            binding.textMissGoal.setTextColor(getResources().getColor(R.color.miss))
+        }
+        fun punch(){
+            binding.btnStart.alpha = 1.0F
+            binding.btnStart.isEnabled = true
+            controlRedBalls(1)
+        }
 
-            rball.background =(getResources().getDrawable(R.drawable.cball))
-            lball.background =(getResources().getDrawable(R.drawable.cball))
-            cball.background =(getResources().getDrawable(R.drawable.cball))
+        binding.btnStart.setOnClickListener {
+            redBallsVisible(1)
+            setRedBackgroundBall()
+            controlRedBalls(52)
 
-            cball.alpha = 0.5F
-            lball.alpha = 0.5F
-            rball.alpha = 0.5F
+            binding.btnStart.isEnabled = false
+            binding.btnStart.alpha = 0.5F
+            binding.leftBallRed.alpha = 0.5F
+            binding.centerBallRed.alpha = 0.5F
+            binding.rightBallRed.alpha = 0.5F
 
-            missOrGoal.text = ""
-            if(pointsSumm%5==0 && pointsSumm!=0){
-                ballLeft.visibility = View.VISIBLE
-                ballRight.visibility = View.VISIBLE
-                ball.visibility = View.VISIBLE
+            if (pointsSumm % 5 == 0 && pointsSumm != 0) {
 
-                val timer = object: CountDownTimer(3000, 1000) {
+                ballsVisible(1)
+                setRedBackgroundBall()
+                controlRedBalls(1)
+                binding.btnStart.isEnabled = false
+
+                val timer = object : CountDownTimer(3000, 1000) {
                     override fun onTick(millisUntilFinished: Long) {}
-
                     override fun onFinish() {
-                        ballLeft.visibility = View.INVISIBLE
-                        ballRight.visibility = View.INVISIBLE
-                        ball.visibility = View.INVISIBLE
-                        cball.background =(getResources().getDrawable(R.drawable.ball))
-                        lball.background =(getResources().getDrawable(R.drawable.ball))
-                        rball.background =(getResources().getDrawable(R.drawable.ball))
-                        pointsSumm+=3
-                        cball.alpha = 1.0F
-                        lball.alpha = 1.0F
-                        rball.alpha = 1.0F
-                        btnStart.alpha = 1.0F
-                        points.text = "MY POINTS IS $pointsSumm"
-                        missOrGoal.text = "BOOONUS GAAAME!"
+                        ballsVisible(52)
+                        win(3)
 
+                        binding.leftBallRed.alpha = 1.0F
+                        binding.centerBallRed.alpha = 1.0F
+                        binding.rightBallRed.alpha = 1.0F
+
+                        binding.leftBallRed.background = (getResources().getDrawable(R.drawable.ball))
+                        binding.centerBallRed.background = (getResources().getDrawable(R.drawable.ball))
+                        binding.rightBallRed.background = (getResources().getDrawable(R.drawable.ball))
                     }
                 }
                 timer.start()
             }
         }
 
-        cball.setOnClickListener {
-            btnStart.alpha = 1.0F
-            if(rand(start, end) == 1 ){
-                cball.background =(getResources().getDrawable(R.drawable.ball))
-                cball.alpha = 1.0F
-                pointsSumm+=1
-                points.text = "MY POINTS IS $pointsSumm"
-                rball.setCardBackgroundColor(getResources().getColor(R.color.miss))
-                lball.setCardBackgroundColor(getResources().getColor(R.color.miss))
-                missOrGoal.text = "Hit"
-                missOrGoal.setTextColor(getResources().getColor(R.color.goal))
-                ball.visibility = View.INVISIBLE
-            }else{
-                cball.background =(getResources().getDrawable(R.drawable.ball))
-                cball.alpha = 1.0F
-                ball.visibility = View.INVISIBLE
-                missOrGoal.text = "Miss"
-                missOrGoal.setTextColor(getResources().getColor(R.color.miss))
-            }}
+        binding.leftBallRed.setOnClickListener {
+            punch()
+            if (rand(start, end) == 1 or 2) {
+                binding.leftBallRed.background = (getResources().getDrawable(R.drawable.ball))
+                binding.leftBallRed.alpha = 1.0F
+                win()
+                binding.centerBall.visibility = View.INVISIBLE
+            } else {
+                binding.leftBallRed.background = (getResources().getDrawable(R.drawable.ball))
+                binding.leftBallRed.alpha = 1.0F
+                binding.centerBall.visibility = View.INVISIBLE
+                lose()
+            }
+        }
 
-        lball.setOnClickListener {
-            btnStart.alpha = 1.0F
-            if(rand(start, end) == 1 ){
-                lball.background =(getResources().getDrawable(R.drawable.ball))
-                lball.alpha = 1.0F
-                pointsSumm+=1
-                points.text = "MY POINTS IS $pointsSumm"
-                rball.setCardBackgroundColor(getResources().getColor(R.color.miss))
-                cball.setCardBackgroundColor(getResources().getColor(R.color.miss))
-                missOrGoal.text = "Hit"
-                missOrGoal.setTextColor(getResources().getColor(R.color.goal))
-                ball.visibility = View.INVISIBLE
-            }else{
-                lball.background =(getResources().getDrawable(R.drawable.ball))
-                lball.alpha = 1.0F
-                ball.visibility = View.INVISIBLE
-                missOrGoal.text = "Miss"
-                missOrGoal.setTextColor(getResources().getColor(R.color.miss))
-            }}
-        rball.setOnClickListener {
-            btnStart.alpha = 1.0F
-            if(rand(start, end) == 1 ){
-                rball.background =(getResources().getDrawable(R.drawable.ball))
-                rball.alpha = 1.0F
-                pointsSumm+=1
-                points.text = "MY POINTS IS $pointsSumm"
-                cball.setCardBackgroundColor(getResources().getColor(R.color.miss))
-                lball.setCardBackgroundColor(getResources().getColor(R.color.miss))
-                missOrGoal.text = "Hit"
-                missOrGoal.setTextColor(getResources().getColor(R.color.goal))
-                ball.visibility = View.INVISIBLE
-            }else{
-                rball.background =(getResources().getDrawable(R.drawable.ball))
-                rball.alpha = 1.0F
-                ball.visibility = View.INVISIBLE
-                missOrGoal.text = "Miss"
-                missOrGoal.setTextColor(getResources().getColor(R.color.miss))
-            }}
-
+        binding.centerBallRed.setOnClickListener {
+            punch()
+            if (rand(start, end) == 1 or 2) {
+                binding.centerBallRed.background = (getResources().getDrawable(R.drawable.ball))
+                binding.centerBallRed.alpha = 1.0F
+                win()
+                binding.leftBall.visibility = View.INVISIBLE
+            } else {
+                binding.centerBallRed.background = (getResources().getDrawable(R.drawable.ball))
+                binding.centerBallRed.alpha = 1.0F
+                binding.centerBall.visibility = View.INVISIBLE
+                lose()
+            }
+        }
+        binding.rightBallRed.setOnClickListener {
+            punch()
+            if (rand(start, end) == 1 or 2) {
+                binding.rightBallRed.background = (getResources().getDrawable(R.drawable.ball))
+                binding.rightBallRed.alpha = 1.0F
+                win()
+                binding.centerBall.visibility = View.INVISIBLE
+            } else {
+                binding.rightBallRed.background = (getResources().getDrawable(R.drawable.ball))
+                binding.rightBallRed.alpha = 1.0F
+                binding.centerBall.visibility = View.INVISIBLE
+                lose()
+            }
+        }
     }
 }
